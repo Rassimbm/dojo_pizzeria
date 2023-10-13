bd4e841a6dc792ad65765ead27374d81a48df974
Rassimbm <rassimbenmhamed@outlook.com>
2023-10-13 18:58:28 -0500

    added a random pick function

--- script.js
+++ script.js
@@ -1,13 +1,62 @@
 console.log("Page loaded...");
 
-function makePizza(crustType,sauceType,cheese,toppings){
+function makePizza(crust,sauce,cheese,topping){
     var pizza = {};
-    pizza.crustType = crustType;
-    pizza.sauceType = sauceType;
+    pizza.crust = crust;
+    pizza.sauce = sauce;
     pizza.cheese = cheese;
-    pizza.toppings = toppings;
+    pizza.topping = topping;
     return pizza;
 }
 
 var order1 = makePizza("thin","marinara","mozzarella",["ground beef","spicy chicken","onions","black olives"]);
 console.log(order1);
+
+var crustType = [
+    "deep dish",
+    "hand tossed",
+    "handmade pan",
+    "crunchy thin"
+]
+
+var sauceType = [
+    "robust tomato",
+    "marinara",
+    "honey BBQ",
+    "garlic parmesan"
+]
+
+var cheeseType = [
+    "mozzarella",
+    "Cheddar",
+    "provolone",
+    "brie"
+]
+
+var toppings = [
+    "jalapeno peppers",
+    "banana peppers",
+    "onions",
+    "black olives",
+    "mushrooms",
+    "spinach",
+    "diced tomatoes"
+]
+
+function randomPick(arr){
+    var i = Math.floor(arr.length * Math.random());
+    return arr[i];
+}
+
+function makeRandomPizza(){
+    var rndmPizza = {};
+    rndmPizza.crustType = randomPick(crustType);
+    rndmPizza.sauceType = randomPick(sauceType);
+    rndmPizza.cheeseType = randomPick(cheeseType);
+    rndmPizza.toppings = randomPick(toppings);
+    return rndmPizza;
+}
+
+var rndmOrder1 = makeRandomPizza();
+
+console.log(rndmOrder1);
